6d2a14d6369b33dac01226443a199735c5681b87
Muhammet Gumus <muhammetgms95@gmail.com>
2025-03-11 14:14:00 +0000

    project folder structure changed

--- skeleton/modelling/modelling.py
+++
@@ -1,9 +0,0 @@
-from model.randomforest import RandomForest
-
-
-def model_predict(data, df, name):
-    # Here we need to call the methods related to the model e.g., random forest 
-
-
-def model_evaluate(model, data):
-    model.print_results(data)
--- skeleton/preprocess.py
+++
@@ -1,7 +0,0 @@
-#Methods related to data loading and all pre-processing steps will go here
-import pandas as pd
-def get_input_data():
-    try:
-        return pd.read_csv('input.csv')
-    except():
-        return pd.read_csv('AppGallery.csv')
